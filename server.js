f1a06188e18ed4f9731f5407e75315bd08c97b66
Adam Shire <shirea@wit.edu>
2018-10-05 11:03:44 -0400

    tests for correct session token headers

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const app = express();
 const morgan = require('morgan')
 var request = require('request');
 require('dotenv').config();
+//require('request-debug')(request);
 
 const SESSION_SECRET = process.env.SESSION_SECRET;
 
 if (process.env.NODE_ENV === "development") {
-  var authData = JSON.stringify({UserId: 'username', Password: 'password', InterfaceId: 'eds_api'});
+  var authData = {UserId: process.env.EDS_USER, Password: process.env.EDS_PASS, InterfaceId: 'eds_api'};
 }
+
+//sessionData names must be capitalized
+var sessionData = {
+	"Profile": "eds_api",
+	"Guest": "Y"
+};
 
 const edsapi = require('./lib/edsapi');
 
@@ -40,12 +47,13 @@ function handleSearchRequest(req, res, next) {
   //var options = getSavedOptsFromRequest(req) || buildAPIRequestOptions(req);
 
   var options = {
-    url: 'https://eds-api.ebscohost.com/edsapi/rest/Search?query-1=AND,' + req.query,
+    url: 'https://eds-api.ebscohost.com/edsapi/rest/Search?query-1=AND,' + req.query.q,
     headers: {
       'Content-Type': 'application/json',
       'x-authenticationToken': req.session.authToken,
-      'x-sessionToken': '6789'
-    }
+      'x-sessionToken': req.session.sessionToken
+    },
+		json: true
   };
 
   request.get(options, function(err, response, body) {
@@ -53,66 +61,102 @@ function handleSearchRequest(req, res, next) {
       console.log(err)
       return;
     }
-    //console.log("before refresh: " + req.session.authToken)
+
     if (response.statusCode === 400) { // if there is a problem with the request
-      //  console.log("search request returned status code: " + response.statusCode)
-      //	console.log("here are the options I'm about to save")
-      //console.log(options)
-      //saveOptsToRequest(req, options)
-      refreshAuthToken(req).then(function authOk() {
-        //console.log("after refresh: " + req.session.authToken)
-        handleSearchRequest(req, res, next) // call function again
-      }).catch(function authKo() {
-        res.status(500).send('something');
-      });
+      refreshAuthToken(req)
+			.then(function() {
+          refreshSessionToken(req)
+					.then(function() {
+						handleSearchRequest(req, res, next)
+        }).catch(next);
+			}).catch(next);
+
+
     } else {
-      res.body = body;
-      next();
-    }
-  });
+			res.body = body;
+			next();
+		}
 
+  });
 };
 
-function refreshAuthToken(req) {
+  function refreshAuthToken(req) {
 
-  var refreshAuthTokenPromise = new Promise(function(resolve, reject) {
-    var options = {
-      url: 'https://eds-api.ebscohost.com/authservice/rest/UIDauth',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': authData.length
-      },
-      json: true,
-      body: authData
-    };
+    var refreshAuthTokenPromise = new Promise(function(resolve, reject) {
+      var options = {
+        url: 'https://eds-api.ebscohost.com/authservice/rest/UIDauth',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': authData.length
+        },
+        json: true,
+        body: authData
+      };
 
-    request.post(options, function(err, response, body) {
-      if (err) {
-        console.log(err)
-        reject(new Error(err))
-      }
+      request.post(options, function(err, response, body) {
+        if (err) {
+          console.log(err)
+          reject(new Error(err))
+        }
 
-      var responseBody = body;
-      //console.log("auth request returned status code: " + response.statusCode)
-      if (response.statusCode === 200) {
-        //console.log("setting authToken to: " + responseBody.AuthToken)
-        req.session.authToken = responseBody.AuthToken;
+        var responseBody = body;
+        //console.log("auth request returned status code: " + response.statusCode)
+        if (response.statusCode === 200) {
+          //console.log("setting authToken to: " + responseBody.AuthToken)
+          req.session.authToken = responseBody.AuthToken;
 
-        //console.log(req.session.authToken)
-        resolve();
-      } else {
-        //console.log('SOMETHING HAPPENED');
-        reject(new Error("SOMETHING!!!"));
-      }
+
+          resolve();
+        } else {
+          //console.log('SOMETHING HAPPENED');
+          reject(new Error(responseBody));
+        }
+      });
+
     });
+    return refreshAuthTokenPromise;
+  };
 
+  function refreshSessionToken(req) {
+
+    var refreshSessionTokenPromise = new Promise(function(resolve, reject) {
+      var options = {
+        url: 'https://eds-api.ebscohost.com/edsapi/rest/CreateSession',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-authenticationToken': req.session.authToken
+        },
+				json: true,
+        body: sessionData
+
+      };
+
+      request.post(options, function(err, response, body) {
+        if (err) {
+          console.log(err)
+          reject(new Error(err))
+        }
+				
+        var responseBody = body;
+
+        if (response.statusCode === 200) {
+
+          req.session.sessionToken = responseBody.SessionToken;
+
+          resolve();
+        } else {
+
+          reject("problem with session request" + responseBody);
+        }
+      });
+
+    });
+    return refreshSessionTokenPromise;
+  };
+
+  app.use('/search', handleSearchRequest);
+
+  app.get('/search', function(req, res, next) {
+    res.json(res.body);
   });
-  return refreshAuthTokenPromise;
-};
-
-app.use('/search', handleSearchRequest);
-
-app.get('/search', function(req, res, next) {
-  res.json(res.body);
-});
-module.exports = app;
+  module.exports = app;
--- test/server.spec.js
+++ test/server.spec.js
@@ -8,41 +8,58 @@ var interceptor = nock('https://eds-api.ebscohost.com').persist();
 var searchResponseSuccess = JSON.stringify({AN: 'donuts'});
 var searchResponseAuthFail = JSON.stringify({ErrorNumber: '104'});
 var searchResponseSessionFail = JSON.stringify({ErrorNumber: '108'});
-var validAuthToken = '12345'
-var validSessionToken = '6789'
+var sessionTokenFail = "bad auth token for session refresh";
+var authTokenSuccess = {
+  AuthToken: '12345'
+}
+var sessionTokenSuccess = {
+  SessionToken: '6789'
+}
+
+describe.only('GET /search', function() {
+  describe('with valid auth data', function() {
+
+    //intercept requet for Auth Token
+    interceptor.post('/authservice/rest/UIDauth').reply(function(uri, requestBody) {
+      return [200, authTokenSuccess];
+    });
+
+    //intercept request for session Token
+    interceptor.post('/edsapi/rest/CreateSession').reply(function(uri, requestBody) {
 
 
+      if (this.req.headers['x-authenticationtoken'] != authTokenSuccess.AuthToken) {
+        return [400, sessionTokenFail];
+      } else {
+        return [200, sessionTokenSuccess];
+      }
+    });
 
-interceptor.get('/edsapi/rest/Search').query(true).reply(function(uri, requestBody) {
+    interceptor.get('/edsapi/rest/Search').query(true).reply(function(uri, requestBody) {
 
-  if (this.req.headers['x-sessiontoken'] != validSessionToken) {
-    return [400, searchResponseSessionFail];
-  } else if (this.req.headers['x-authenticationtoken'] != validAuthToken) {
-    return [400, searchResponseAuthFail];
-  } else
-    return [200, searchResponseSuccess];
-    //TODO: if request body has good session token return 200, searchResponse otherwise return 400
-  }
-);
+      if (this.req.headers['x-authenticationtoken'] != authTokenSuccess.AuthToken) {
+        return [400, searchResponseSessionFail];
+      } else if (this.req.headers['x-sessiontoken'] != sessionTokenSuccess.SessionToken) {
+        return [400, searchResponseAuthFail];
+      } else
+        return [200, searchResponseSuccess];
+        //TODO: if request body has good session token return 200, searchResponse otherwise return 400
+      }
+    );
 
-var authResponse = {
-  'AuthToken': '12345'
-};
-interceptor.post('/authservice/rest/UIDauth').reply(200, authResponse);
+    it('success', function(done) {
 
-describe('GET /search', function() {
-  it('success', function(done) {
+      request(server).get('/search')
+      //.expect('Content-Type', /json/)
+        .expect(200).expect(function(res) {
+        //console.log(res)
+        expect(res).to.exist;
+        expect(searchResponseSuccess)
+      }).end(done);
 
-
-    request(server).get('/search')
-    //.expect('Content-Type', /json/)
-    .expect(200)
-    .expect(function(res) {
-    //console.log(res)
-      expect(res).to.exist;
-      expect(searchResponseSuccess)
-    }).end(done);
+    });
 
   });
+
 
 });
